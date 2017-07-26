c82937658f29c32eac411b4b069f97657d12ab32
unknown <mgnjulien@gmail.com>
2017-07-26 15:46:35 +0200

    fix little bullshit in my code :)

--- DoomKernel.c
+++ DoomKernel.c
@@ -8,14 +8,12 @@ extern doom8 kattr;
 
 void _start(){
 	
-        kY = 3;
+        kY = 1;
         kattr = 0x04;
         putsDoom("Welcome to the DoomKernel\n");
 
         kattr = 0x4E;
         putsDoom("Prepare to die ! I have the PainSaw little shit !\n");
-
-        scrollupDoom(2);
 
         while (1);
 }
